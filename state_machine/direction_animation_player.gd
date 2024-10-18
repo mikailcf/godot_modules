d3760677338ae9358ffdc2ab0452bcb4ad450c8e
Mikail Freitas <mikailcf@gmail.com>
2024-10-18 07:18:10 -0300

    removing replay parameter from FSM animation player

--- state_machine/direction_animation_player.gd
+++ state_machine/direction_animation_player.gd
@@ -2,7 +2,7 @@ extends AnimationPlayer
 
 class_name DirectionAnimationPlayer
 
-func handle_direction(direction: Vector2, replay := false):
+func handle_direction(direction: Vector2):
 	pass
 
 func play_animation(animation: String):
