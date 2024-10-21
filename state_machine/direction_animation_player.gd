94405ade0ca5ffaa81b70be34ab4960de911e510
Mikail Freitas <mikailcf@gmail.com>
2024-10-21 13:15:49 -0300

    state machine animation player speed alpha

--- state_machine/direction_animation_player.gd
+++ state_machine/direction_animation_player.gd
@@ -5,7 +5,7 @@ class_name DirectionAnimationPlayer
 func handle_direction(_direction: Vector2):
 	pass
 
-func play_animation(_animation: String):
+func play_animation(_animation: String, _speed: float = 1.0):
 	pass
 
 func turn_to(_direction: Vector2):
