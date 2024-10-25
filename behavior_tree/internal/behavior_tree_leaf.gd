be3db8dc2256647713ee14bb3c524651589ac63f
Mikail Freitas <mikailcf@gmail.com>
2024-10-24 22:43:24 -0300

    letting behavior tree leaves have children that are godot nodes

--- behavior_tree/internal/behavior_tree_leaf.gd
+++ behavior_tree/internal/behavior_tree_leaf.gd
@@ -2,5 +2,11 @@ class_name BehaviorTreeLeaf
 extends BehaviorTreeNode
 
 func _ready():
-	assert(get_child_count() == 0,
-		"A Leaf node cannot have children")
+	var children = get_children()
+	var behavior_tree_children_count = 0
+	
+	for child in children:
+		if child is BehaviorTreeNode:
+			behavior_tree_children_count += 1
+	
+	assert(behavior_tree_children_count == 0, "A Leaf node cannot have children")
